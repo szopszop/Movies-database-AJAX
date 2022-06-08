bed3c9147faa800358199e56a3751e4c1291b101
Szymon Tracz <szymontracz1@gmail.com>
2022-06-08 18:03:02 +0200

    almost second

--- data/queries.py
+++ data/queries.py
@@ -3,3 +3,33 @@ from data import data_manager
 
 def get_shows():
     return data_manager.execute_select('SELECT id, title FROM shows;')
+
+
+def get_single_show(show_id):
+    return data_manager.execute_select("""
+    SELECT shows.id, shows.title, EXTRACT(YEAR FROM shows.year) AS year,
+    shows.runtime,ROUND(shows.rating, 1) AS rating,
+    STRING_AGG(genres.name, ', ' ORDER BY genres.name) AS genre,
+    shows.trailer, shows.homepage
+    FROM shows
+    JOIN show_genres ON shows.id = show_genres.show_id
+    JOIN genres ON show_genres.genre_id = genres.id
+    WHERE show_id = %(show_id)s
+    GROUP BY  shows.id, shows.rating
+    ORDER BY shows.rating DESC
+    ;""", show_id, fetchall=False)
+
+
+def get_most_rated_shows():
+    return data_manager.execute_select("""
+    SELECT shows.id, shows.title, EXTRACT(YEAR FROM shows.year) AS year,
+    shows.runtime,ROUND(shows.rating, 1) AS rating,
+    STRING_AGG(genres.name, ', ' ORDER BY genres.name) AS genre,
+    shows.trailer, shows.homepage
+    FROM shows
+    JOIN show_genres ON shows.id = show_genres.show_id
+    JOIN genres ON show_genres.genre_id = genres.id
+    GROUP BY  shows.id, shows.rating
+    ORDER BY shows.rating DESC
+    LIMIT 15
+    ;""")
--- main.py
+++ main.py
@@ -18,8 +18,20 @@ def design():
     return render_template('design.html')
 
 
+@app.route('/shows/most-rated')
+def most_rated():
+    shows = queries.get_most_rated_shows()
+    return render_template('most-rated.html', shows=shows)
+
+
+@app.route('/show/<id_>')
+def single_show(id_):
+    show = queries.get_single_show(id_)
+    return render_template('single-show.html', show=show)
+
+
 def main():
-    app.run(debug=False)
+    app.run(debug=True)
 
 
 if __name__ == '__main__':
